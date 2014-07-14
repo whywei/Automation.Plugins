71067039c63499376ccc6ca92131a966304c99dc
git1207 <git1302@163.com>
2014-07-14 11:08:07 +0800

    [车载] 上传车载图标

--- Automation.Plugins.YZ.Vehicular/Action/VehicularAction.cs
+++ Automation.Plugins.YZ.Vehicular/Action/VehicularAction.cs
@@ -28,12 +28,12 @@ namespace Automation.Plugins.YZ.Vehicular.Action
         public override void Activate()
         {
             IHeaderControl header = App.HeaderControl;
-            header.Add(new SimpleActionItem(rootKey, "自动", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
-            header.Add(new SimpleActionItem(rootKey, "手动", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
-            header.Add(new SimpleActionItem(rootKey, "申请", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
-            header.Add(new SimpleActionItem(rootKey, "取消", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
-            header.Add(new SimpleActionItem(rootKey, "完成", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
-            header.Add(new SimpleActionItem(rootKey, "批量", (sender, e) => { }) { SmallImage = Resources.cmd_test_16x16, LargeImage = Resources.cmd_test_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "自动", (sender, e) => { }) { SmallImage = Resources.run_right_16x16, LargeImage = Resources.run_right_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "手动", (sender, e) => { }) { SmallImage = Resources.hand_16x16, LargeImage = Resources.hand_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "申请", (sender, e) => { }) { SmallImage = Resources.apply_16x16, LargeImage = Resources.apply_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "取消", (sender, e) => { }) { SmallImage = Resources.cannel_16x16, LargeImage = Resources.cannel_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "完成", (sender, e) => { }) { SmallImage = Resources.ok_16x16, LargeImage = Resources.ok_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "批量", (sender, e) => { }) { SmallImage = Resources.batch_16x16, LargeImage = Resources.batch_32x32 });
 
             dropItem = new DropDownActionItem() { RootKey = rootKey, GroupCaption = "选择作业类型", Width = 170 , MultiSelect = true};
 
