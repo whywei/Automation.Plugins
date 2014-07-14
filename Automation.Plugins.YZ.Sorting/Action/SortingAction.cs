0bb7a4447e5592747582d2733100c2fa409fd269
git1207 <git1302@163.com>
2014-07-14 15:43:32 +0800

    [分拣] 上传分拣模块图标

--- Automation.Plugins.YZ.Sorting/Action/SortingAction.cs
+++ Automation.Plugins.YZ.Sorting/Action/SortingAction.cs
@@ -29,19 +29,19 @@ namespace Automation.Plugins.YZ.Sorting.Action
         {
             IHeaderControl header = App.HeaderControl;
 
-            header.Add(new SimpleActionItem(rootKey, "数据下载", DataDownLoad_click) { ToolTipText = "数据下载", GroupCaption = "操作", SortOrder = 1, LargeImage = Resources.Customer_Query_32 });
-            header.Add(new SimpleActionItem(rootKey, "开始分拣", StartSort_Click) { ToolTipText = "开始分拣", GroupCaption = "操作", SortOrder = 2, LargeImage = Resources.HandleStock_32 });
-            header.Add(new SimpleActionItem(rootKey, "停止分拣", StopSort_click) { ToolTipText = "停止分拣", GroupCaption = "操作", SortOrder = 3, LargeImage = Resources.Customer_Query_32 });
+            header.Add(new SimpleActionItem(rootKey, "数据下载", DataDownLoad_click) { ToolTipText = "数据下载", GroupCaption = "操作", SortOrder = 1, LargeImage = Resources.download_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "开始分拣", StartSort_Click) { ToolTipText = "开始分拣", GroupCaption = "操作", SortOrder = 2, LargeImage = Resources.start_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "停止分拣", StopSort_click) { ToolTipText = "停止分拣", GroupCaption = "操作", SortOrder = 3, LargeImage = Resources.spouse_32x32 });
 
-            header.Add(new SimpleActionItem(rootKey, "烟道查询", ChannelQuery_Click) { ToolTipText = "分拣烟道盘点", GroupCaption = "查询", SortOrder = 1, LargeImage = Resources.SortChannelCheck_32 });
-            header.Add(new SimpleActionItem(rootKey, "烟包查询", PackNoQuery_Click) { ToolTipText = "分拣订单查询", GroupCaption = "查询", SortOrder = 2, LargeImage = Resources.Sorting_Query_32 });
-            header.Add(new SimpleActionItem(rootKey, "客户查询", CustomerQuery_Click) { ToolTipText = "客户订单查询", GroupCaption = "查询", SortOrder = 3, LargeImage = Resources.Customer_Query_32 });
+            header.Add(new SimpleActionItem(rootKey, "烟道查询", ChannelQuery_Click) { ToolTipText = "分拣烟道盘点", GroupCaption = "查询", SortOrder = 1, LargeImage = Resources.yandao_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "烟包查询", PackNoQuery_Click) { ToolTipText = "分拣订单查询", GroupCaption = "查询", SortOrder = 2, LargeImage = Resources.package_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "客户查询", CustomerQuery_Click) { ToolTipText = "客户订单查询", GroupCaption = "查询", SortOrder = 3, LargeImage = Resources.customer_32x32 });
             header.Add(new SimpleActionItem(rootKey, "缓存订单", CacheOrderQuery_Click) { ToolTipText = "缓存订单查询", GroupCaption = "查询", SortOrder = 4, LargeImage = Resources.CacheOrderQuery_32 });
-            header.Add(new SimpleActionItem(rootKey, "下单记录", SortingRecordQuery_Click) { ToolTipText = "下单记录查询", GroupCaption = "查询", SortOrder = 5, LargeImage = Resources.CacheOrderQuery_32 });
-            header.Add(new SimpleActionItem(rootKey, "手工补货", HandSuppyQuery_Click) { ToolTipText = "手工补货查询", GroupCaption = "查询", SortOrder = 6, LargeImage = Resources.CacheOrderQuery_32 });
-            header.Add(new SimpleActionItem(rootKey, "包装机数据", PackDataQuery_Click) { ToolTipText = "包装机数据查询", GroupCaption = "查询", SortOrder = 7, LargeImage = Resources.CacheOrderQuery_32 });
-            header.Add(new SimpleActionItem(rootKey, "分拣进度", SortingProgressQuery_Click) { ToolTipText = "分拣进度查询", GroupCaption = "查询", SortOrder = 8, LargeImage = Resources.CacheOrderQuery_32 });
-            header.Add(new SimpleActionItem(rootKey, "分拣效率", SortingEfficiencyQuery_Click) { ToolTipText = "分拣效率查询", GroupCaption = "查询", SortOrder = 9, LargeImage = Resources.CacheOrderQuery_32 });
+            header.Add(new SimpleActionItem(rootKey, "下单记录", SortingRecordQuery_Click) { ToolTipText = "下单记录查询", GroupCaption = "查询", SortOrder = 5, LargeImage = Resources.Sorting_Query_32 });
+            header.Add(new SimpleActionItem(rootKey, "手工补货", HandSuppyQuery_Click) { ToolTipText = "手工补货查询", GroupCaption = "查询", SortOrder = 6, LargeImage = Resources.handwork_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "包装机数据", PackDataQuery_Click) { ToolTipText = "包装机数据查询", GroupCaption = "查询", SortOrder = 7, LargeImage = Resources.package_data_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "分拣进度", SortingProgressQuery_Click) { ToolTipText = "分拣进度查询", GroupCaption = "查询", SortOrder = 8, LargeImage = Resources.bar_32x32 });
+            header.Add(new SimpleActionItem(rootKey, "分拣效率", SortingEfficiencyQuery_Click) { ToolTipText = "分拣效率查询", GroupCaption = "查询", SortOrder = 9, LargeImage = Resources.Efficiency_32x32 });
         }
 
         private void DataDownLoad_click(object sender, EventArgs e)
