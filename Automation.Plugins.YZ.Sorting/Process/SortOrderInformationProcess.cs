c4690bf8df81cef3e7dfd084502a6821223dc0c8
zqb <940658539@qq.com>
2014-07-16 11:59:18 +0800

    永州分拣监控系统 添加线程文件

---
+++ Automation.Plugins.YZ.Sorting/Process/SortOrderInformationProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Automation.Core;
+
+namespace Automation.Plugins.YZ.Sorting.Process
+{
+    public class SortOrderInformationProcess : AbstractProcess
+    {
+        public override void Initialize()
+        {
+            Description = "分拣订单信息处理线程";
+            base.Initialize();
+        }
+
+        public override void Execute()
+        {
+        }
+    }
+}
