0d64ce488a344feb5162276bfb2500c551b2d4a8
git1207 <git1302@163.com>
2014-08-06 17:08:13 +0800

    [全部作业]添加GridControl控件

--- Automation.Plugins.YZ.ManualSupply/View/Controls/AllTaskControl.cs
+++ Automation.Plugins.YZ.ManualSupply/View/Controls/AllTaskControl.cs
@@ -12,9 +12,28 @@ namespace Automation.Plugins.YZ.ManualSupply.View.Controls
 {
     public partial class AllTaskControl : DevExpress.XtraEditors.XtraUserControl
     {
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+
         public AllTaskControl()
         {
             InitializeComponent();
+            this.CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            gridView1.OptionsView.ColumnAutoWidth = false;
+            gridView1.OptionsView.ShowGroupPanel = false;
+
+            gridColumn1 = gridView1.Columns.AddVisible("supply_id", "补货编号");
+            gridColumn1 = gridView1.Columns.AddVisible("supply_batch", "批次");
+            gridColumn1 = gridView1.Columns.AddVisible("pack_no", "包号");
+            gridColumn1 = gridView1.Columns.AddVisible("channel_code", "烟道编码");
+            gridColumn1 = gridView1.Columns.AddVisible("product_code", "商品编码");
+            gridColumn1 = gridView1.Columns.AddVisible("product_name", "商品名称");
+            gridColumn1 = gridView1.Columns.AddVisible("quantity", "数量");
+            gridColumn1 = gridView1.Columns.AddVisible("status", "状态");
         }
     }
 }
