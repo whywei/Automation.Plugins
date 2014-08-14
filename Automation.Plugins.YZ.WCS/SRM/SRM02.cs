faa148a3483cf2a896a78abc2e02e9eb7b7e3b4b
zengys <ynnuyonw@126.com>
2014-08-14 16:59:45 +0800

    更新 Automation.Common.SRM ，升级  nuget 包 到  1.0.0.4.

---
+++ Automation.Plugins.YZ.WCS/SRM/SRM02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Automation.Common.SRM;
+
+namespace Automation.Plugins.YZ.WCS.SRM
+{
+    [Export(typeof(ISRM))]
+    public class SRM02 : AbstractSRM
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Name = "2号堆垛机";
+        }
+
+        protected override SRMTask ApplyNewTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool CancelCurrentTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void FinishCurrentTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
