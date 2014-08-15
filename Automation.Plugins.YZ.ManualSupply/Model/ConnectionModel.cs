9b101fc5b4560b2b493f134343baba0745e452a3
zqb <940658539@qq.com>
2014-08-15 09:49:05 +0800

    永州手工补货系统 实现参数设置

---
+++ Automation.Plugins.YZ.ManualSupply/Model/ConnectionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Automation.Plugins.YZ.ManualSupply.Model
+{
+    public class ConnectionModel
+    {
+        public string IP { get; set; }
+        public string DataBaseName { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
