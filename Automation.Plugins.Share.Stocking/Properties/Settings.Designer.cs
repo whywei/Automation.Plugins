6b8f173cb4a2d0c48874e8954f3be772703a7feb
zengys <ynnuyonw@126.com>
2014-09-13 17:35:34 +0800

    调整相关配置。

--- Automation.Plugins.Share.Stocking/Properties/Settings.Designer.cs
+++ Automation.Plugins.Share.Stocking/Properties/Settings.Designer.cs
@@ -42,7 +42,7 @@ namespace Automation.Plugins.Share.Stocking.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("11")]
         public string HttpUrl {
             get {
                 return ((string)(this["HttpUrl"]));
