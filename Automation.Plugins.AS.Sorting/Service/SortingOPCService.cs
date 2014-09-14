482158a19813c6a5863058ef6ba22188b9269949
zengys <ynnuyonw@126.com>
2014-09-14 08:59:10 +0800

    调整统一分拣线编号配置。

--- Automation.Plugins.AS.Sorting/Service/SortingOPCService.cs
+++ Automation.Plugins.AS.Sorting/Service/SortingOPCService.cs
@@ -6,11 +6,15 @@ using Automation.Core;
 using Automation.Service.OPC;
 using System.IO;
 using Automation.Plugins.AS.Sorting.Properties;
+using System.ComponentModel.Composition;
 
 namespace Automation.Plugins.AS.Sorting.Service
 {
     public class SortingOPCService : AbstractServiceWrapper
     {
+        [Import("SortLineCode", typeof(string))]
+        public string SortLineCode { get; set; }
+
         public override void Initialize()
         {
             Description = "分拣OPC服务[AB]";
@@ -20,7 +24,7 @@ namespace Automation.Plugins.AS.Sorting.Service
         public override void CreateTargetService()
         {
             Name = "SortPLC";
-            this.TargetService = new OPCService(GetConfigFilePath().Replace(".xml", string.Format("[{0}].xml", Settings.Default.SortLineCode)));
+            this.TargetService = new OPCService(GetConfigFilePath().Replace(".xml", string.Format("[{0}].xml", SortLineCode)));
         }
     }
 }
--- Automation.Plugins.Share.Sorting/Global.cs
+++ Automation.Plugins.Share.Sorting/Global.cs
@@ -1,8 +1,13 @@
-﻿
+﻿using System.ComponentModel.Composition;
+using Automation.Plugins.Share.Sorting.Properties;
+
 namespace Automation.Plugins.Share.Sorting
 {
     public class Global
     {
+       [Export("SortLineCode", typeof(string))]
+       private static string SortLineCode = Settings.Default.Sorting_Line_Code;
+
        public const string SERVER_DATABASE_NAME = "ServerDB";
        public const string SORTING_DATABASE_NAME = "SortDB";
        public const string PLC_SERVICE_NAME = "SortPLC";
