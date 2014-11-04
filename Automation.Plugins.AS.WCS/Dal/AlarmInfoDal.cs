1fac54de0a7a4c6b6ffc66cdc15acb202cfb0cdb
zqb <940658539@qq.com>
2014-11-04 15:01:57 +0800

    安顺WCS加入堆垛机报警信息显示

---
+++ Automation.Plugins.AS.WCS/Dal/AlarmInfoDal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DBRabbit;
+using Automation.Core;
+
+namespace Automation.Plugins.AS.WCS.Dal
+{
+    public class AlarmInfoDal : AbstractBaseDal
+    {
+        public FaultAlarmInfo[] SelectAlarmInfoByAlarmCode(IList<int> AlarmCodes)
+        {
+            IList<FaultAlarmInfo> faultAlarmInfos = new List<FaultAlarmInfo>();
+            if (AlarmCodes.Count > 0)
+            {
+                string AlarmCodeStr = "('";
+                foreach (var a in AlarmCodes)
+                {
+                    AlarmCodeStr += a.ToString() + "','";
+                }
+                AlarmCodeStr = AlarmCodeStr.Remove(AlarmCodeStr.LastIndexOf("','"), 2);
+                AlarmCodeStr += ")";
+                var ra = TransactionScopeManager[Global.SERVER_DATABASE_NAME].NewRelationAccesser();
+                string sql = string.Format(@"select * from wcs_alarm_info where alarm_code in {0}", AlarmCodeStr);
+                var set = ra.DoQuery(sql);
+
+                for (int i = 0; i < set.Tables[0].Rows.Count; i++)
+                {
+                    FaultAlarmInfo FaultAlarmInfo = new FaultAlarmInfo();
+                    FaultAlarmInfo.FaultCode = set.Tables[0].Rows[i]["alarm_code"].ToString();
+                    FaultAlarmInfo.Description = set.Tables[0].Rows[i]["description"].ToString();
+                    faultAlarmInfos.Add(FaultAlarmInfo);
+                }
+            }
+
+            return faultAlarmInfos.ToArray();
+        }
+    }
+}
---
+++ Automation.Plugins.AS.WCS/FaultAlarm/SRMFaultAlarm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Automation.Core;
+using Automation.Plugins.AS.WCS.Dal;
+using System.ComponentModel.Composition;
+using Automation.Plugins.Common.SRM;
+using Automation.Plugins.AS.WCS.SRM;
+
+namespace Automation.Plugins.AS.WCS.FaultAlarm
+{
+    public class SRMFaultAlarm : AbstractFaultAlarm
+    {
+        [Import(typeof(ISRM))]
+        public SRM01 Srm01 { get; set; }
+
+        public override void Refresh()
+        {
+            SRM01 srm = new SRM01();
+            try
+            {
+                IList<int> alarmCodes = null;
+                FaultAlarmInfo[] faultAlarmInfos = null;
+                AlarmInfoDal alarmInfoDal = new AlarmInfoDal();
+                alarmCodes = Srm01.AlarmCodes;
+                faultAlarmInfos = alarmInfoDal.SelectAlarmInfoByAlarmCode(alarmCodes);
+                AutomationContext.RefreshFaultAlarm("SRM01", faultAlarmInfos);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("FaultAlarmProcess 出错，原因：" + ex.Message + "/n" + ex.StackTrace);
+            }
+        }
+
+        public override void Reset()
+        {
+        }
+    }
+}
