c357344f4f640dc09a9074bbd5ebbe0d64330134
zengys <ynnuyonw@126.com>
2014-11-21 17:22:52 +0800

    整理各项目解决方案目录。

--- Automation.Plugins.YZ.Forklift/Properties/AssemblyInfo.cs
+++ Automation.Plugins.YZ.Forklift/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Automation.Plugins.YB.ManualSupply")]
+[assembly: AssemblyTitle("Automation.Plugins.YZ.Forklift")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Automation.Plugins.YB.ManualSupply")]
+[assembly: AssemblyProduct("Automation.Plugins.YZ.Forklift")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8184f8f8-a155-44f6-aca1-8db3e8cf43f4")]
+[assembly: Guid("23bb01f0-1122-4d33-b03c-c5c93aa285bc")]
 
 // 程序集的版本信息由下面四个值组成:
 //
